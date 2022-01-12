3af9b41f3c8c4d259574c0c8f07c635640808ec3
gongchangyou <gongchangyou@58.com>
2022-01-12 16:47:35 +0800

    service

--- service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
+++ service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
@@ -2,12 +2,12 @@ package com.braindata.dubbodemo.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.braindata.dubbodemo.intf.StuRpcService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value="/pay")
+//@RestController
+//@RequestMapping(value="/pay")
+@Deprecated
 public class PayController {
     @Reference
     StuRpcService stuRpcService;
