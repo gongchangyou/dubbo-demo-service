b555c305f5340681689eb6c424a8bbb6d69fb971
gongchangyou <gongchangyou@58.com>
2022-01-13 09:21:15 +0800

    init contract

--- service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
+++ service/src/main/java/com/braindata/dubbodemo/controller/PayController.java
@@ -5,16 +5,16 @@ import com.braindata.dubbodemo.intf.StuRpcService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping(value="/pay")
+@RestController
+@RequestMapping(value="/pay")
 @Deprecated
 public class PayController {
-    @Reference
-    StuRpcService stuRpcService;
+//    @Reference
+//    StuRpcService stuRpcService;
 
     @RequestMapping(value = "/toPay")
     public String pay(){
-        stuRpcService.add(3, 4);
+//        int a = stuRpcService.add(3, 4);
         return "success!";
     }
 }
--- service/src/main/java/com/braindata/dubbodemo/impl/StuRpcServiceImpl.java
+++ service/src/main/java/com/braindata/dubbodemo/impl/StuRpcServiceImpl.java
@@ -1,14 +1,15 @@
 package com.braindata.dubbodemo.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.braindata.dubbodemo.intf.StuRpcService;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
  * @author gongchangyou
  * @version 1.0
  * @date 2022/1/12 9:57 上午
  */
-@Service
+@DubboService(version = "1.0.0",group = "stu-rpc-server")
 public class StuRpcServiceImpl implements StuRpcService {
 
     @Override
